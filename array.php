e9eaab70f106be60abb543c71190ab6f625a026d
dakankro <henriquedavi0876@gmail.com>
2025-11-03 11:30:25 -0300

    dfk

--- array.php
+++ array.php
@@ -4,3 +4,33 @@
     $mes = [
         'Janeiro','Fevereiro','Março','Abril','Maio','Junho', 'Julho', 'Agosto','Setembro','Outubro', 'Novembro','Dezembro'
     ];
+
+    foreach ($mes as $key => $value) {
+        echo $key . ': ' . $value . '<br>';
+    }
+
+    echo '<hr>';
+    echo 'VAR_DUMP';
+    var_dump($mes);
+    echo '<hr>';
+    echo 'posição  ';
+    echo '<br>';
+    echo $mes[2];
+    echo '<hr>';
+
+    function dataAtual(): string
+    {
+        $diaMes = date('d');
+        $diaSemana = date('w');
+        $mes = date('n');
+        $ano = date('Y');
+
+        $nomeDosMeses = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho', 'Julho', 'Agosto','Setembro','Outubro', 'Novembro','Dezembro'
+        ];
+        $nomeDiaSemana = ['Domingo','Segunda-feira','Terça-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'];
+        $dataAtual =  $nomeDiaSemana[$diaSemana] . ';'.$diaMes.' de ' . $nomeDosMeses[$mes] . '/' . $ano . '.';
+
+        return $dataAtual;
+    }
+    echo dataAtual();
+
--- contar_tempo/contar_tempo].php
+++ contar_tempo/contar_tempo].php
@@ -1,8 +1,23 @@
 <?php
-function contarTempo(string $data) : string
-{
-     $agora = strtotime(date('Y-m-d H:i:s'));
-     $tempo = strtotime($data);
+function contarTempo(string $data) : string {
+    $agora = strtotime(date('Y-m-d H:i:s'));
+    $tempo = strtotime($data);
 
 
+    echo('Data do PC ' . $agora);
+    echo "<hr>";
+    echo('Data digitada ' . $tempo);
+    $diference = $agora - $tempo;
+    echo "<hr>";
+    echo('Diferença ' . $diference);
+
+    $segundos = $diference;
+    $minutos = round(num: $diference / 60);// um minuto tem 60
+    $horas = round(num: $diference / 3600);// uma hora tem 60 minutos
+    $dias = round(num: $diference / 86400);// um dia tem 24 horas
+    $semanas = round(num: $diference / 604800);// uma semana tem sete dias
+    $meses = round(num: $diference / 2419200);// um mês tem quatr semanas
+    $anos = round(num: $diference / 29030400);// um ano tem doze meses
+
+    return $tempo;
 }
